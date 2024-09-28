d5c691ea7590c9e200a9e271f84e6950ad848ed6
Andreas Tsaridas <andreas.tsaridas@gmail.com>
2024-09-28 16:39:20 +0000

    added service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ManifestService } from './manifest.service';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http'; // Ensure this import
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, HttpClientModule], // Add HttpClientModule here
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers: [ManifestService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'src';
+  manifestData: any;
+
+  constructor(private manifestService: ManifestService) {}
+
+  ngOnInit(): void {
+    this.manifestService.getManifest().subscribe(data => {
+      this.manifestData = data;
+    });
+  }
 }
